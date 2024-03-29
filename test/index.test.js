0c88752430394dfad5c6e7120c10a79672dce449
Onyx Vega <mabs.24gl@gmail.com>
2024-03-29 13:13:58 -0400

    tried to setup test, needs work

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,12 @@
+const Triangle = require('../index.js');
 
-
-const shape = new Triangle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+describe('Triangle', () => {
+    describe('Triangle', () => {
+        it('should take new shape and color and render svg', () => {
+            const shape = new Triangle();
+            shape.setColor("blue");
+            expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />')
+        });
+    });
+});
 
