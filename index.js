85cfd330278c77474dc8059c96bed89b3c9f2389
Onyx Vega <mabs.24gl@gmail.com>
2024-04-01 19:42:34 -0400

    reorganized code to allow for tests

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const inquirer = require('inquirer');
-const Sequelize = require("sequelize");
-const fs = require('fs');
+const { Triangle, Square, Circle } = require('./lib/shape.js');
 
-
-const userColorsText = () => {
-    inquirer
+function run() {
+    return inquirer
         .prompt([
+            {
+                type: 'list',
+                message: 'Choose a shape:',
+                name: 'shape',
+                choices: ['triangle', 'circle', 'square'],
+            },
             {
                 type: 'input',
                 message: 'What do you want your logo to say?',
@@ -23,89 +27,22 @@ const userColorsText = () => {
             },
         ])
         .then((answers) => {
-            let text = answers.text;
-            let color = answers.color;
-            let color2 = answers.color2;
-            
-            const userShape = () => {
-                
-                inquirer
-                .prompt([
-                    {
-                        type: 'list',
-                        message: 'Choose a shape:',
-                        name: 'shape',
-                        choices: ['triangle', 'circle', 'square'],
-                    },
-                ])
-                .then((choices) => {
-                    let shape = choices.shape;
-                    if (shape === "triangle") {
-                        const Triangle = () => {                            
-                            const logo = 
-                            `<!DOCTYPE html>
-                            <html>
-                            <body>                             
-                            <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                            <polygon points="149,10 298,199 10,199" fill="${color2}"/>
-                            <text font-size="60" x="50%" y="50%" dominant-baseline="hanging" text-anchor="middle" fill="${color}">${text}</text>
-                            </svg>                             
-                            </body>
-                            </html>`;
-                              
-                             fs.writeFile('logo.svg', logo, (err) =>
-                             err ? console.log(err) : console.log('Generated logo.svg')
-                             );
-                        };
-                         Triangle();
-                    }
-
-                    if (shape === "square") {
-                        const Square = () => {
-                            const logo =
-                            `<!DOCTYPE html>
-                            <html>
-                            <body>
-                            <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">                           
-                            <rect width="200" height="200" x="10" y="10" fill="${color2}"/>
-                            <text font-size="50" x="50%" y="50%" dominant-baseline="central" text-anchor="end" fill="${color}">${text}</text>
-                            </svg>
-                            </body>
-                            </html>`;
-    
-                            fs.writeFile('logo.svg', logo, (err) =>
-                            err ? console.log(err) : console.log('Generated logo.svg')
-                            );
-                        };
-                        Square();
-                    }
-
-                    if (shape === "circle") {
-                        const Circle = () => {
-                            const logo =
-                                `<!DOCTYPE html>
-                                <html>
-                                <body>                                    
-                                <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                                <circle r="100" cx="150" cy="100" fill="${color2}"/>
-                                <text font-size="50" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${color}">${text}</text>
-                                </svg>                                    
-                                </body>
-                                </html>`;
-                             
-                            fs.writeFile('logo.svg', logo, (err) =>
-                                err ? console.log(err) : console.log('Generated logo.svg')
-                            );
-                        };
-                        Circle();
-                    }
-                });
+            let shape = answers.shape;
+            if (shape === "triangle") {
+                let triangleGen = new Triangle(answers.text, answers.color, answers.color2);
+                triangleGen.render();
             }
-            userShape();           
+
+            if (shape === "square") {
+                let squareGen = new Square(answers.text, answers.color, answers.color2);
+                squareGen.render();
+            }
+
+            if (shape === "circle") {
+                let circleGen = new Circle(answers.text, answers.color, answers.color2);
+                circleGen.render();
+            }
         });
-}
-userColorsText();
+};
 
-
-
-
+run();
